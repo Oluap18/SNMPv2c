7a9b096530caf274f9d9622f2cc0c5baf66b4dde
Paulo Guedes <paulojorge.guedes@hotmail.com>
2018-02-11 05:45:15 +0000

    Feito o processo de codificadação e descodificação

--- encode/varBindList.c
+++
@@ -1,5 +0,0 @@
-#include "varBindList.h"
-
-void addVarBind(VarBindList_t* varlist, VarBind_t* var_bind){
-	int r = ASN_SEQUENCE_ADD(&varlist->list, var_bind);
-}
--- encode/varBindList.h
+++
@@ -1,9 +0,0 @@
-#ifndef	_VarBind_List_H_
-#define	_VarBind_List_H_
-
-#include "VarBindList.h"
-#include "VarBind.h"
-
-void addVarBind(VarBindList_t* varlist, VarBind_t* var_bind);
-
-#endif
